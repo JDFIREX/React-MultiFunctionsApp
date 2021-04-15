39944434c677d18dedc5fad85f4ef200091d3e2d
JDFIREX <42634156+JDFIREX@users.noreply.github.com>
2021-04-15 19:34:15 +0200

    .

--- src/App/Location/Location.js
+++ src/App/Location/Location.js
@@ -1,14 +1,42 @@
-import React, {useContext} from "react"
+import React, {useContext,useEffect} from "react"
 import {Context} from "./../../UseReducer"
+import mapboxgl from "mapbox-gl"
+import country from "./../../countries.json"
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import "./Location.css"
 
+let map;
 
 const Location = () => {
+
+    useEffect(() => {
+
+        mapboxgl.accessToken = 'pk.eyJ1IjoiamRmaXJleCIsImEiOiJja2JmZXBhY3QwdWw2MnNxZTlnY2N1ZDM4In0.Q34Lq3f1apoEpcn8teup1w';
+
+        map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11'
+        });
+        console.log(country)
+
+        // Add the control to the map.
+        map.addControl(
+            new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                mapboxgl: mapboxgl
+            })
+        );
+
+
+    },[])
 
     const [state,dispatch] = useContext(Context)
 
     return (
-        <div className="Location">
-            Location
+        <div className="Location" >
+            <div id="map">
+            </div>
         </div>
     )
 
