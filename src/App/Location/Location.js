aac47b2ce630b39fe7a19ee4a92e0d2feb138547
JDFIREX <42634156+JDFIREX@users.noreply.github.com>
2021-04-16 18:00:05 +0200

    .

--- src/App/Location/Location.js
+++ src/App/Location/Location.js
@@ -1,41 +1,55 @@
-import React, {useContext,useEffect} from "react"
+import React, {useContext,useEffect, useRef, useState} from "react"
 import {Context} from "./../../UseReducer"
 import mapboxgl from "mapbox-gl"
-import country from "./../../countries.json"
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+// import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
+// import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import "./Location.css"
 
-let map;
+// mapboxgl.workerClass = MapboxWorker;
+mapboxgl.accessToken = 'pk.eyJ1IjoiamRmaXJleCIsImEiOiJja2JmZXBhY3QwdWw2MnNxZTlnY2N1ZDM4In0.Q34Lq3f1apoEpcn8teup1w';
 
 const Location = () => {
 
+    const mapContainer = useRef();
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+
     useEffect(() => {
-
-        mapboxgl.accessToken = 'pk.eyJ1IjoiamRmaXJleCIsImEiOiJja2JmZXBhY3QwdWw2MnNxZTlnY2N1ZDM4In0.Q34Lq3f1apoEpcn8teup1w';
-
-        map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11'
+        const map = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11'
         });
-        console.log(country)
 
-        // Add the control to the map.
-        map.addControl(
-            new MapboxGeocoder({
-                accessToken: mapboxgl.accessToken,
-                mapboxgl: mapboxgl
-            })
-        );
+        map.on('move', () => {
+            setLng(map.getCenter().lng.toFixed(4));
+            setLat(map.getCenter().lat.toFixed(4));
+            setZoom(map.getZoom().toFixed(2));
+        });
 
+        let geocoder = new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            marker: {
+                color: 'orange'
+            },
+            mapboxgl: mapboxgl
+        });
 
+        map.addControl(geocoder);
+
+        return () => map.remove();
     },[])
 
     const [state,dispatch] = useContext(Context)
 
     return (
         <div className="Location" >
-            <div id="map">
+            <div className="sidebar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div id="map" ref={mapContainer}>
             </div>
         </div>
     )
